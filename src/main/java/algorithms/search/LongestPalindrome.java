dd1e706867ca0347b64382f5549664e7d08fa3ab
Steve Reed <steven.m.reed@gmail.com>
2010-05-02 11:40:34 -0700

    Palindrome works, now needs cleanup.

--- src/main/java/algorithms/search/LongestPalindrome.java
+++ src/main/java/algorithms/search/LongestPalindrome.java
@@ -1,11 +1,16 @@
 package algorithms.search;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Supplier;
 import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.Lists;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -58,7 +63,7 @@ public class LongestPalindrome {
         };
     }
 
-    private int longestPalindromeCenteredOnIndexInString(Integer index, CharSequence string, int minLength) {
+    private int longestPalindromeCenteredOnIndexInString(final Integer index, final CharSequence string, int minLength) {
         if ((minLength / 2) > index) {
             return -1;
         }
@@ -66,42 +71,78 @@ public class LongestPalindrome {
             return -1;
         }
 
+        final AtomicInteger l = new AtomicInteger();
         SortedSet<Integer> lengths = new TreeSet<Integer>();
-
-        boolean oddHit = true;
-        boolean evenHit = true;
-
-        for (int i = 1; isWithinBounds(i, index, string); i++) {
-
-            // even length
-            if (evenHit && string.charAt(index - i) != string.charAt(index + i - 1)) {
-                evenHit = false;
-                int length = (i - 1) * 2;
-                if (length >= minLength) {
-                    lengths.add(length);
-                }
+        Supplier<Integer> evenSupplier = new Supplier<Integer>() {
+            boolean enabled = true;
+            @Override
+            public Integer get() {
+                enabled = enabled && (string.charAt(index - l.get()) == string.charAt(index + l.get() - 1));
+                return enabled
+                        ? 2 * l.get()
+                        : -1;
             }
-            // odd length
-            if (oddHit && string.charAt(index - i) != string.charAt(index + i)) {
-                oddHit = false;
-                int length = ((i - 1) * 2) + 1;
-                if (length >= minLength) {
-                    lengths.add(length);
-                }
+        };
+        Supplier<Integer> oddSupplier = new Supplier<Integer>() {
+            boolean enabled = true;
+            @Override
+            public Integer get() {
+                enabled = enabled && (string.charAt(index - l.get()) == string.charAt(index + l.get()));
+                return enabled
+                        ? 2 * l.get() + 1
+                        : -1;
             }
+        };
 
-            if (!(evenHit || oddHit)) {
-                if (lengths.isEmpty()) {
-                    return -1;
-                } else {
-                    return lengths.last();
-                }
+        for (Integer length : lengthsToTry(index, string)) {
+            l.set(length);
+            int e = evenSupplier.get();
+            int o = oddSupplier.get();
+            if (e == o && e == -1) {
+                break;
             }
+            lengths.add(e);
+            lengths.add(o);
         }
 
-        int i = Math.min(index, string.length() - index - 1);
-        i = (i * 2) + (i % 2);
-        return i;
+        return lengths.isEmpty() ? -1 : lengths.last();
+    }
+
+    private Iterable<Integer> lengthsToTry(final int index, final CharSequence string) {
+        return new Iterable<Integer>() {
+            @Override
+            public Iterator<Integer> iterator() {
+                return new AbstractIterator<Integer>() {
+
+                    private int i = 1;
+
+                    @Override
+                    protected Integer computeNext() {
+                        if (wouldExtendPastBeginning(i)) {
+                            return endOfData();
+                        } else if (wouldExtendPastEnd(i)) {
+                            return endOfData();
+                        } else {
+                            return i++;
+                        }
+                    }
+
+                    private boolean wouldExtendPastEnd(int i) {
+                        if (i % 2 == 0) {
+                            // even
+                            return index - 1 + i >= string.length();
+                        } else {
+                            // odd
+                            return index + i >= string.length();
+                        }
+                    }
+
+                    private boolean wouldExtendPastBeginning(int i) {
+                        return index - i < 0;
+                    }
+                };
+            }
+        };
     }
 
     private boolean isWithinBounds(int distanceFromOddCenter, Integer index, CharSequence string) {
